4d378f26bd16d1b4bc40aeaf708f01d2600a1cf2
jgagarin <jon.gagarin@gmail.com>
2015-02-23 15:06:43 -0800

    functions added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-for(var num = 1; num <= 100; num++){
+var maxNumber = prompt("Suggest a Max Number");
+
+function fizzBuzz(n){
+    
+    for(var num = 1; num <= n; num++){
     
     if(num%3 == 0 && num%5 == 0){   
-    console.log("Fizz Buzz");
     $('.fizzbuzz').append('<li>Fizz Buzz</li>');    
     } else if (num%5 == 0){
-    console.log("Buzz");
     $('.fizzbuzz').append('<li>Buzz</li>');    
     }else if (num%3 == 0) {
-    console.log("Fizz");
     $('.fizzbuzz').append('<li>Fizz</li>');    
     } else {
-    console.log(num)
     var fizzval = "<li>"+num+"</li>";
     $('.fizzbuzz').append(fizzval);    
     };
 
-};
+ }
+}
+
+fizzBuzz(maxNumber);
+
 
 
 
